9f4066fa5f641e2f93b7b68f234fc1b0954813f2
Upenha <contato.upenha@gmail.com>
2022-12-15 01:13:28 -0300

    team only

--- source/commands/Admin/eval.ts
+++ source/commands/Admin/eval.ts
@@ -5,6 +5,7 @@ import { getEmoji } from '../../utils/getEmoji';
 
 @ApplyOptions<Command.Options>({
 	description: 'Eval!',
+  preconditions: ['TeamOnly'],
 })
 
 export class UserCommand extends Command {
