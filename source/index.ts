77b1b1a2de9919f4e7b0406fa55ad11d954f0f9f
upenha <44807913+upenha@users.noreply.github.com>
2023-01-05 23:42:07 -0300

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -3,6 +3,5 @@ import { red } from 'colorette';
 import { textSync } from 'figlet';
 import { Client } from './structures/Client';
 const client = new Client();
-// console.log(red(textSync('DUKHAN', 'Big')))
 
 client.login(process.env.DISCORD_TOKEN);
